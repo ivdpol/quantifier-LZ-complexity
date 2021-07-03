ce93cf95a8c6cc869c1bced0dae9cc412d506987
Iris van de Pol <i.p.a.vandepol@uva.nl>
2021-07-03 14:31:27 +0200

    Correct license statement
    License statement referred to old repository name. Replaced with new repository name.

--- util.py
+++ util.py
@@ -1,14 +1,14 @@
 """
 Copyright 2018 Iris van de Pol and Shane Steinert-Threlkeld.
 
-This file is part of learn-complex.
+This file is part of quantifier-LZ-complexity.
 
-learn-complex is free software: you can redistribute it and/or modify
+quantifier-LZ-complexity is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-learn-complex is distributed in the hope that it will be useful,
+quantifier-LZ-complexity is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
